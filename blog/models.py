83461f39fd7e2a8a659e40e9be7269c48ea1adb2
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-04 12:41:23 +0000

    Added slug, post_id, and author fields to Post model

--- blog/models.py
+++ blog/models.py
@@ -11,3 +11,13 @@ class Post(models.Model):
         max_length=200,
         unique=True
         )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True
+        )
+    post_id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+        )
