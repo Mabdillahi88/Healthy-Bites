f3f59fc41e5190a8f75f0b8f137dd3b4a6570514
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-07 20:24:43 +0000

    Updated Post admin: field settings, filters, display, and search

--- blog/admin.py
+++ blog/admin.py
@@ -7,5 +7,9 @@ from .models import Post, Comment
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content')
+    list_filter = ('status', 'created_date')
+    list_display = ('title', 'slug', 'status', 'created_date')
+    search_fields = ['title', 'content']
 
-git
