8c9f5da3ad76f3ac413b5d387180b6d7d763e441
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-10 16:30:23 +0000

    updated blog post template

--- blog/models.py
+++ blog/models.py
@@ -2,35 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-
 STATUS = ((0, 'Draft'), (1, 'Posted'))
 
-
-
 class Post(models.Model):
-    title = models.CharField(
-        max_length=200,
-        unique=True
-        )
-    slug = models.SlugField(
-        max_length=200,
-        unique=True
-        )
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
     post_id = models.AutoField(primary_key=True)
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='blog_posts'
-        )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     created_date = models.DateTimeField(blank=True)
     updated_date = models.DateTimeField()
     content = models.TextField()
-    featured_image = CloudinaryField(
-        'image',
-        default='placeholder'
-        )
+    featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='liked_posts')  
 
     class Meta:
         ordering = ['-created_date']
@@ -41,25 +26,13 @@ class Post(models.Model):
     def like_count(self):
         return self.likes.count()
 
-
-
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE,
-        related_name='comments'
-        )
-    name = models.CharField(
-        max_length=50
-        )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=50)
     email = models.EmailField()
     body = models.TextField()
-    created_date = models.DateTimeField(
-        auto_now_add=True
-        )
-    approved = models.BooleanField(
-        default=False
-        )
+    created_date = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ['created_date']
