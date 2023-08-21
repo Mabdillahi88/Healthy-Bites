4cbcf59841c0dc18de067542a4d55af5605f646a
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-21 17:17:14 +0000

    Initialized a BookingForm class with Crispy Forms helper for enhanced rendering.

--- booking/forms.py
+++ booking/forms.py
@@ -4,3 +4,11 @@ from crispy_forms.layout import Layout, Submit, Row, Column
 from datetime import datetime
 from phonenumber_field.formfields import PhoneNumberField
 from .models import Booking
+
+# This form is used for users to make a booking at our restaurant.
+class BookingForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        
