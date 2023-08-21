10ce2fd950d72d73bd94c59e44c455773e567a22
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-21 06:17:51 +0000

    Implement UserReservationRequest view with GET, POST and booking form logic.

--- booking/views.py
+++ booking/views.py
@@ -14,3 +14,21 @@ import datetime
 class UserReservationRequestView(LoginRequiredMixin, View):
     template_name = 'initiate_reservation_page.html'
     success_message = 'Booking successful, awaiting confirmation.'
+
+    def get_booking_form(self, request):
+        initial_data = {'email': request.user.email} if request.user.is_authenticated else {}
+        return BookingForm(initial=initial_data)
+
+    def get(self, request, *args, **kwargs):
+        booking_form = self.get_booking_form(request)
+        return render(request, self.template_name, {'booking_form': booking_form})
+
+    def post(self, request, *args, **kwargs):
+        booking_form = BookingForm(data=request.POST)
+        if booking_form.is_valid():
+            booking = booking_form.save(commit=False)
+            booking.user = request.user
+            booking.save()
+            messages.success(request, self.success_message)
+            return redirect('acknowledgment_view')
+        return render(request, self.template_name, {'booking_form': booking_form})
