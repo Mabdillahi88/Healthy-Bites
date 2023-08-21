d6b833f36fd6d61acdfe79bd0457812e65ed005f
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-21 06:33:26 +0000

    Define Table model with name and seating capacity attributes.

--- booking/models.py
+++ booking/models.py
@@ -13,3 +13,16 @@ statuses = [
     'Booking Expired'
 ]
 status_options = tuple((status.lower(), status) for status in statuses)
+
+class Table(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, default='New Table', unique=True, help_text="The name of the table.")
+    max_seats = models.PositiveIntegerField(default=2, help_text="Max seating capacity of the table.")
+
+    class Meta:
+        # Order tables by their seating capacity (descending).
+        ordering = ['-max_seats']
+
+    def __str__(self):
+        return self.name
