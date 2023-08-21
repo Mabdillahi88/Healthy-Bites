c51cca353553c9dae4beea9a0793ec2ed3fa0da7
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-21 06:39:38 +0000

    Integrate reservation desk section in template extending from base.html.

--- booking/models.py
+++ booking/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import User
 
-
+# Define available booking time slots from 12:00 to 23:00.
 time_slots = tuple((f"{hour:02}:00", f"{hour:02}:00") for hour in range(12, 24))
 
-
+# Define potential statuses for a booking.
 statuses = [
     'Awaiting Confirmation',
     'Booking Confirmed',
@@ -15,7 +15,9 @@ statuses = [
 status_options = tuple((status.lower(), status) for status in statuses)
 
 class Table(models.Model):
-
+    """
+    Represents a table in the restaurant.
+    """
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, default='New Table', unique=True, help_text="The name of the table.")
     max_seats = models.PositiveIntegerField(default=2, help_text="Max seating capacity of the table.")
@@ -28,19 +30,21 @@ class Table(models.Model):
         return self.name
 
 class Booking(models.Model):
-
+    """
+    Represents a customer's booking in the restaurant.
+    """
     id = models.AutoField(primary_key=True)
     created_date = models.DateTimeField(auto_now_add=True, help_text="Timestamp when the booking was created.")
     requested_date = models.DateField(help_text="Desired date for the reservation.")
     requested_time = models.CharField(max_length=25, choices=time_slots, default='17:00', help_text="Desired time for the reservation.")
     
- 
+    # Table that's being booked.
     table = models.ForeignKey(Table, on_delete=models.CASCADE, related_name="reservations", null=True)
     
-
+    # User who made the booking.
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookings", null=True)
     
-    
+    # Customer details.
     name = models.CharField(max_length=50, null=True)
     email = models.EmailField(max_length=254, default="")
     phone = PhoneNumberField(null=True)
