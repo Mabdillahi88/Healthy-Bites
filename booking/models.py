7932f5374bacb982f738ae9d9a3fbd6921ebb255
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-21 06:31:35 +0000

    Initialize time slots and booking status options.

--- booking/models.py
+++ booking/models.py
@@ -4,3 +4,12 @@ from django.contrib.auth.models import User
 
 
 time_slots = tuple((f"{hour:02}:00", f"{hour:02}:00") for hour in range(12, 24))
+
+
+statuses = [
+    'Awaiting Confirmation',
+    'Booking Confirmed',
+    'Booking Rejected',
+    'Booking Expired'
+]
+status_options = tuple((status.lower(), status) for status in statuses)
