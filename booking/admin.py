3ed226761dbf6694724acff5a70a1d69d7d6d756
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-21 17:39:59 +0000

    Enhance TableAdmin for better visibility and management of restaurant tables in the admin interface.

--- booking/admin.py
+++ booking/admin.py
@@ -5,3 +5,18 @@ from .models import Table, Booking
 # Admin configuration for managing tables in the restaurant.
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
+    """
+    Admin interface for managing tables. Allows for easy viewing of table details
+    and searching based on table names.
+    """
+    # Fields to display in the list view, changing the order for distinctiveness
+    list_display = ('name', 'id', 'max_seats')
+    
+    # Fields that can be used for search in the admin panel
+    search_fields = ('name', 'id')
+
+    # Fields that can be used to filter the displayed list, added 'id' for filtering
+    list_filter = ('id', 'max_seats')
+
+    # Default ordering based on maximum seats
+    ordering = ('max_seats', 'name')
