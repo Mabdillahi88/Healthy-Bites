fd0812fd6880be66319d3b9ad6b68afbda7d0107
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-21 06:15:19 +0000

    Refactor imports in views and  add booking functionalities.

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.views.generic import ListView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.core.exceptions import PermissionDenied
+from django.contrib.auth.models import User
+from .models import Booking
+from .forms import BookingForm
+import datetime
