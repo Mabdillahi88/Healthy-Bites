a484188b39855b986925e5fb05d3ce8143e01032
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-21 11:16:58 +0000

    Added URL patterns for booking views.

--- booking/urls.py
+++ booking/urls.py
@@ -2,3 +2,10 @@ from django.urls import path
 # Internal:
 from booking import views
 
+urlpatterns = [
+    path('initiate_reservation_page', views.UserReservationRequestView.as_view(), name='initiate_reservation_page'),
+    path('acknowledgment_view', views.BookingAcknowledgmentPageView.as_view(), name='acknowledgment_view'),
+    path('active_bookings_dashboard', views.UserSpecificBookingsDashboardView.as_view(), name='active_bookings_dashboard'),
+    path('modify_reservation_form/<int:pk>', views.IndividualBookingModificationView.as_view(), name='modify_reservation_form'),
+    path('terminate_reservation_dialog/<int:pk>', views.SpecificBookingTerminationView.as_view(), name='terminate_reservation_dialog'),
+]
