b31aa422b307f843eefe9c3130bf2c0c11bee2c0
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-21 17:39:16 +0000

    Implement admin panel configuration for managing restaurant tables.

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin, messages
 from rangefilter.filters import DateRangeFilter
 from .models import Table, Booking
+
+# Admin configuration for managing tables in the restaurant.
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
