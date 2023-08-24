10d30f40c76f2215d67b5359c52f70f89e23dd86
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-24 15:28:12 +0000

    Created a Django form for capturing contact details and messages.

--- contact/forms.py
+++ contact/forms.py
@@ -2,3 +2,9 @@ from django import forms
 from crispy_forms.helper import FormHelper
 from phonenumber_field.formfields import PhoneNumberField
 from .models import MyContact
+
+class ContactForm(forms.ModelForm):
+    """
+    A form to capture contact details and messages.
+    Uses crispy_forms for improved frontend presentation.
+    """
