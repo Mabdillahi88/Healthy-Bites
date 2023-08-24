14660f5a5bd04a0513546cb7140b5063c9040e03
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-24 15:28:47 +0000

    Added international format placeholder to the phone field in the contact form.

--- contact/forms.py
+++ contact/forms.py
@@ -8,3 +8,8 @@ class ContactForm(forms.ModelForm):
     A form to capture contact details and messages.
     Uses crispy_forms for improved frontend presentation.
     """
+
+    # Phone field with international format placeholder
+    phone = PhoneNumberField(
+        widget=forms.TextInput(attrs={'placeholder': '+353123456789'})
+    )
