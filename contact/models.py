779ff78d6bb20dceea2f9bf2f233dd2d4e48088c
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-24 15:15:09 +0000

    Created the MyContact model with the message_id field.

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import User
+
+class MyContact(models.Model):
+    """
+    Represents a contact entry with associated user details and message.
+    """
+    
+    # Unique identifier for the contact
+    message_id = models.AutoField(primary_key=True)
+    
