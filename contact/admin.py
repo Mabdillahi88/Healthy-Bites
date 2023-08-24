df1b52f4bd9966e5837edf35c5c41989624504cc
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-24 15:23:22 +0000

    Registered MyContact  model with Django Admin and configured display fields.

--- contact/admin.py
+++ contact/admin.py
@@ -2,3 +2,16 @@
 from django.contrib import admin
 from rangefilter.filters import DateRangeFilter
 from .models import MyContact
+
+# Registering the MyContact model with Django Admin
+@admin.register(MyContact)
+class ContactAdmin(admin.ModelAdmin):
+    # Fields to display in the admin list view
+    list_display = (
+        'message_id',
+        'user',
+        'name',
+        'message',
+        'phone',
+        'created_date'
+    )
