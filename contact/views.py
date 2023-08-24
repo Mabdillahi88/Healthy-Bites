366c9716a7023aff21abd3daf50e02f7a023bf26
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-24 14:58:44 +0000

    Added a function to fetch the user instance based on the current session.

--- contact/views.py
+++ contact/views.py
@@ -3,3 +3,14 @@ from django.views import View
 from django.contrib.auth.models import User
 from django.contrib import messages
 from .forms import ContactForm
+
+def get_user_instance(request):
+    """
+    Fetch the user instance based on the current session.
+
+    Parameters:
+    - request: The HTTP request object.
+
+    Returns:
+    - User instance or None if not found.
+    """
