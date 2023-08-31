749c1e8449d67d0cee225bde0b892c467b4f747d
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-31 02:12:38 +0000

    Add view to display food menu

--- menu/views.py
+++ menu/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from .models import MealOption, Beverage
 
 def display_food(request):
-   
+    """
+    a view to display the food menu
+    """
+    meal_items = MealOption.objects.all()
+    return render(request, 'food_menu.html', {'food_items': meal_items})
