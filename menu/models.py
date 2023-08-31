e132da1176020a1338f945977e6ee1a1e203191a
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-31 02:20:54 +0000

    Added fields and methods to the Beverage model.

--- menu/models.py
+++ menu/models.py
@@ -32,3 +32,21 @@ class MealOption(models.Model):
 
     def __str__(self):
         return self.meal_name
+
+class Beverage(models.Model):
+    """
+    A class for the beverage options, contains
+    smoothie blends, shake varieties, and new beverages.
+    """
+    beverage_id = models.AutoField(primary_key=True)  # Explicit primary key
+    beverage_name = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    price = models.FloatField()
+    beverage_category = models.IntegerField(choices=BEVERAGE_CATEGORIES, default=2)
+    available = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-available']
+
+    def __str__(self):
+        return self.beverage_name
