99fda31d944bd8d3c67890875dc9150927be248f
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-31 02:22:52 +0000

    Registered MealOption model and enabled Summernote editor for description.

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import MealOption, Beverage
+
+
+@admin.register(MealOption)
+class MealOptionAdmin(SummernoteModelAdmin):
+    summernote_fields = ('description',)  
