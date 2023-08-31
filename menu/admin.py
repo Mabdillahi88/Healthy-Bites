06194337a7ca031429fcb9bf00e18d8691a00819
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-31 02:23:38 +0000

    Configured MealOptionAdmin with list display, search fields, and filters.

--- menu/admin.py
+++ menu/admin.py
@@ -6,3 +6,7 @@ from .models import MealOption, Beverage
 @admin.register(MealOption)
 class MealOptionAdmin(SummernoteModelAdmin):
     summernote_fields = ('description',)  
+
+    list_display = ('meal_name', 'meal_category', 'price', 'available')
+    search_fields = ('meal_name', 'description')
+    list_filter = ('available', 'meal_category')
