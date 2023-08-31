d0ad236847ae570807db540bb4a119bdbacb1813
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-31 02:37:34 +0000

    Included URL configuration for the menu app.

--- healthy_bites/urls.py
+++ healthy_bites/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('accounts/', include("allauth.urls")),
     path('booking/', include('booking.urls')), 
     path('', include('contact.urls')),
+    path('', include('menu.urls')),
     path('', include("blog.urls")),
 ]
