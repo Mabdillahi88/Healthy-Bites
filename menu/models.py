4d25a567a7815f0a810021fa38d628af9362264a
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-31 02:18:01 +0000

    Defined the MealOption model class to represent different meal options.

--- menu/models.py
+++ menu/models.py
@@ -14,3 +14,39 @@ BEVERAGE_CATEGORIES = (
     (1, 'Shake Varieties'),
     (2, 'New')
 )
+
+class MealOption(models.Model):
+    """
+    A class for the meal options model, contains
+    starters, mains, desserts, and new arrivals.
+    """
+    meal_id = models.AutoField(primary_key=True)  # Explicit primary key
+    meal_name = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    price = models.FloatField()
+    meal_category = models.IntegerField(choices=FOOD_CATEGORIES, default=3)
+    available = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-meal_category']
+
+    def __str__(self):
+        return self.meal_name
+
+class Beverage(models.Model):
+    """
+    A class for the beverage options, contains
+    smoothie blends, shake varieties, and new beverages.
+    """
+    beverage_id = models.AutoField(primary_key=True)  # Explicit primary key
+    beverage_name = models.CharField(max_length=50, unique=True)
+    description = models.TextField()
+    price = models.FloatField()
+    beverage_category = models.IntegerField(choices=BEVERAGE_CATEGORIES, default=2)
+    available = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-available']
+
+    def __str__(self):
+        return self.beverage_name
