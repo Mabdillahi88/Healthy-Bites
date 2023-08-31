7d2ca83e45084fe64ef2742368d9c24c7a382c13
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-31 02:41:05 +0000

    Configured URLs for displaying food menu, smoothie blends, and shake varieties in the menu app.

--- menu/urls.py
+++ menu/urls.py
@@ -2,3 +2,8 @@ from django.urls import path
 from menu import views
 
 
+urlpatterns = [
+    path('food_menu/', views.display_food, name='food_menu'),
+    path('smoothies/', views.display_smoothies, name='smoothies'),
+    path('milkshakes/', views.display_shakes, name='milkshakes'),
+]
