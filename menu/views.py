232fae2428c276b783e8b6cee4cb0d120af4d099
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-08-31 02:14:36 +0000

    Implemented view function to display shake varieties menu with filtering by beverage category.

--- menu/views.py
+++ menu/views.py
@@ -14,3 +14,10 @@ def display_smoothies(request):
     """
     blends_list = Beverage.objects.filter(beverage_category=0)
     return render(request, 'smoothies.html', {'blends_list': blends_list})
+
+def display_shakes(request):
+    """
+    a view to showcase the shake varieties menu
+    """
+    shake_list = Beverage.objects.filter(beverage_category=1)
+    return render(request, 'milkshakes.html', {'shake_list': shake_list})
